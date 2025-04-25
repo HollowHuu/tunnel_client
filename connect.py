58a342baead911a65b0a34ce543e4bd646254f88
HollowHuu <nico@mmx.dk>
2025-04-25 07:27:07 +0200

    moonraker test

--- connect.py
+++ connect.py
@@ -1,36 +1,53 @@
 import asyncio
 import websockets
+from websockets.asyncio.client import connect
 import aiohttp
 import json
 
+import moonraker
+
 RELAY_URL = "ws://localhost:9001"
+MOONRAKER_URL = "ws://192.168.0.129:7125/klippysocket"
 
 
 class ServerConnection:
-    def __init__(self, ws):
-        self.ws = ws
+    def __init__(self):
         self.connected = True
-    
-    async def send_printer_info(ws: websockets.ClientConnection):
+
+    async def send_printer_info(self, ws: websockets.ClientConnection):
+        print("Sending printer info...")
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(
-                    f"{RELAY_URL}", # Just for testing
-                    headers={"Content-Type": "application/json"},
-                    json={"info": "Printer information"}
-                ) as response:
-                    if response.status == 200:
-                        print("Printer info sent successfully")
-                    else:
-                        print(f"Failed to send printer info: {response.status}")
+            # Simulate sending printer info
+            printer_info = {
+                "status": "online",
+                "temperature": 200,
+                "progress": 50
+            }
+            await ws.send(json.dumps(printer_info))
+            print("Printer info sent")
+
+            message = await ws.recv()
+            print(f"Received message: {message}")
         except Exception as e:
             print(f"Error: {e}")
 
 async def main():
-    async with websockets.connect(RELAY_URL) as ws:
+
+
+    moonraker_connection = moonraker.Moonraker(MOONRAKER_URL)
+    moonraker_task = asyncio.create_task(moonraker_connection.connect())
+    await moonraker_task
+
+    moonraker_task = asyncio.create_task(moonraker_connection.listen())
+    await moonraker_task
+
+    async with connect(RELAY_URL) as ws:
+        await ws.send("Hello from client")
         try:
+            server_connection = ServerConnection()
             print("Connected to relay")
-            asyncio.create_task(handle_messages(ws))
+            task = asyncio.create_task(server_connection.send_printer_info(ws))
+            await task
         except websockets.ConnectionClosed:
             print("Connection closed")
 
